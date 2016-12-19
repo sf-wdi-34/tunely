c30d149fcbb709cefc59bcd2ad699c829f2a82cd
nzoLogic <nzoLogic@gmail.com>
2016-12-19 12:01:32 -0800

    step 5 complete. we got function routes yall

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     app = express(),
-    db = require('./models');
+    db = require('./models'),
+    controllers = require('./controllers');
 
   app.use(express.static('public'));
 
@@ -8,7 +9,7 @@ app.get('/', function(req, res){
   console.log(__dirname);
   res.sendFile('views/index.html', { root: __dirname});
 });
-
+app.get('/api', controllers.api.index);
 app.listen(process.env.port || 3000, function(){
   console.log('express server online on port ', 3000);
 });
